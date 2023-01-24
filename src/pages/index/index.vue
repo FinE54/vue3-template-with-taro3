3b48535c8574cb31b8799c844bc3985ce089bcfa
Fine <finegn@outlook.com>
2023-01-25 01:52:24 +0800

    update: 首页增加 WindiCSS 样式

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,15 +1,11 @@
 <template>
-  <div>
-    <span>{{ msg }}</span>
+  <div class="py-12 text-center">
+    <span class=" text-2xl text-blue-400">{{ msg }}</span>
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
 
-const msg = ref("Hello world");
-
-return {
-  msg,
-};
+const msg = ref("Hello Taro3");
 </script>
